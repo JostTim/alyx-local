8853e8f8716d3dde5352e7c48e38225835f3dbda
Timothe Jost <timothe.jost@wanadoo.fr>
2023-05-17 21:49:10 +0200

    reversing yes no to make sense

--- alyx/data/admin.py
+++ alyx/data/admin.py
@@ -92,9 +92,9 @@ class IsOnlineListFilter(SimpleListFilter):
     def queryset(self, request, queryset):
         value = self.value()
         if value == 'Yes':
-            return queryset.filter(file_records__gt=0)
-        elif value == 'No':
             return queryset.exclude(file_records__gt=0)
+        elif value == 'No':
+            return queryset.filter(file_records__gt=0)
         return queryset
 
 class DatasetAdmin(BaseExperimentalDataAdmin):
