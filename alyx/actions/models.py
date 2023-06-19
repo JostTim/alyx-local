3a3d2cd77518c8f3251458385805695e6bddd319
Timothe Jost <timothe.jost@wanadoo.fr>
2023-06-19 21:09:19 +0200

    test

--- alyx/actions/models.py
+++ alyx/actions/models.py
@@ -356,7 +356,9 @@ class Session(BaseAction):
 
     @property
     def path(self):
-        return os.path.join( self.default_data_repository.data_path , self.alias )
+        from django.apps import apps
+        DataRepository = apps.get_model('data', 'DataRepository')
+        return os.path.join( DataRepository.objects.filter(pk=self.default_data_repository).first().data_path , self.alias )
 
 class EphysSession(Session):
     """
