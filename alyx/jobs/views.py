e94ce785b9264ddd3465576fc980493c3350b607
Timothe Jost <timothe.jost@wanadoo.fr>
2023-12-20 17:55:30 +0100

    test of task logs view

--- alyx/jobs/views.py
+++ alyx/jobs/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q, Count, Max
 from rest_framework import generics
 from django_filters.rest_framework import CharFilter
 from django.views.generic.list import ListView
+from django.views.generic.base import View
 
 import numpy as np
 
@@ -15,42 +16,44 @@ from actions.models import Session
 
 
 class TasksStatusView(ListView):
-    template_name = 'tasks.html'
+    template_name = "tasks.html"
     paginate_by = 50
 
     def get_context_data(self, **kwargs):
-        graph = self.kwargs.get('graph', None)
+        graph = self.kwargs.get("graph", None)
         context = super(TasksStatusView, self).get_context_data(**kwargs)
-        context['tableFilter'] = self.f
-        context['graphs'] = list(Task.objects.all().values_list('graph', flat=True).distinct())
+        context["tableFilter"] = self.f
+        context["graphs"] = list(Task.objects.all().values_list("graph", flat=True).distinct())
         # annotate the labs for template display
-        cw = Count('session__tasks', filter=Q(session__tasks__status=20))
-        ls = Max('session__start_time', filter=Q(session__tasks__isnull=False))
-        lj = Max('session__tasks__datetime')
-        context['labs'] = Lab.objects.annotate(
-            count_waiting=cw, last_session=ls, last_job=lj).order_by('name')
-        space = np.array(context['labs'].values_list(
-            'json__raid_available', flat=True), dtype=np.float)
-        context['space_left'] = np.round(space / 1000, decimals=1)
-        context['ibllib_version'] = list(context['labs'].values_list(
-            'json__ibllib_version', flat=True))
+        cw = Count("session__tasks", filter=Q(session__tasks__status=20))
+        ls = Max("session__start_time", filter=Q(session__tasks__isnull=False))
+        lj = Max("session__tasks__datetime")
+        context["labs"] = Lab.objects.annotate(count_waiting=cw, last_session=ls, last_job=lj).order_by("name")
+        space = np.array(context["labs"].values_list("json__raid_available", flat=True), dtype=np.float)
+        context["space_left"] = np.round(space / 1000, decimals=1)
+        context["ibllib_version"] = list(context["labs"].values_list("json__ibllib_version", flat=True))
         if graph:
             # here the empty order_by is to fix a low level SQL bug with distinct when called
             # on value lists and unique together constraints. bof.
             # https://code.djangoproject.com/ticket/16058
-            context['task_names'] = list(
-                Task.objects.filter(graph=graph).exclude(session__qc=50).order_by(
-                    "-priority").order_by("level").order_by().values_list(
-                    'name', flat=True).distinct())
+            context["task_names"] = list(
+                Task.objects.filter(graph=graph)
+                .exclude(session__qc=50)
+                .order_by("-priority")
+                .order_by("level")
+                .order_by()
+                .values_list("name", flat=True)
+                .distinct()
+            )
         else:
-            context['task_names'] = []
-        context['title'] = 'Tasks Recap'
-        context['site_header'] = 'Alyx'
+            context["task_names"] = []
+        context["title"] = "Tasks Recap"
+        context["site_header"] = "Alyx"
         return context
 
     def get_queryset(self):
-        graph = self.kwargs.get('graph', None)
-        lab = self.kwargs.get('lab', None)
+        graph = self.kwargs.get("graph", None)
+        lab = self.kwargs.get("lab", None)
         qs = Session.objects.exclude(qc=50)
         if lab is None and graph is None:
             qs = Session.objects.none()
@@ -58,33 +61,34 @@ class TasksStatusView(ListView):
             if lab:
                 qs = qs.filter(lab__name=lab)
             if graph:
-                qs = qs.filter(tasks__graph=self.kwargs['graph'])
+                qs = qs.filter(tasks__graph=self.kwargs["graph"])
 
         self.f = ProjectFilter(self.request.GET, queryset=qs)
 
-        return self.f.qs.distinct().order_by('-start_time')
+        return self.f.qs.distinct().order_by("-start_time")
 
 
 class ProjectFilter(django_filters.FilterSet):
     """
     Filter used in combobox of task admin page
     """
+
     class Meta:
         model = Session
-        fields = ['projects']
-        exclude = ['json']
+        fields = ["projects"]
+        exclude = ["json"]
 
     def __init__(self, *args, **kwargs):
         super(ProjectFilter, self).__init__(*args, **kwargs)
 
 
 class TaskFilter(BaseFilterSet):
-    lab = CharFilter('session__lab__name')
-    status = CharFilter(method='enum_field_filter')
+    lab = CharFilter("session__lab__name")
+    status = CharFilter(method="enum_field_filter")
 
     class Meta:
         model = Task
-        exclude = ['json']
+        exclude = ["json"]
 
 
 class TaskList(generics.ListCreateAPIView):
@@ -97,7 +101,8 @@ class TaskList(generics.ListCreateAPIView):
 
     [===> task model reference](/admin/doc/models/jobs.task)
     """
-    queryset = Task.objects.all().order_by('level', '-priority', '-session__start_time')
+
+    queryset = Task.objects.all().order_by("level", "-priority", "-session__start_time")
     serializer_class = TaskSerializer
     permission_classes = rest_permission_classes()
     filter_class = TaskFilter
@@ -107,3 +112,15 @@ class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = rest_permission_classes()
+
+
+class TaskLogs(View):
+    template_name = "task_logs.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        task_id = self.kwargs.get("task_id", None)
+        ansi_logging_content = open("/mnt/one/cajal2/ONE/Adaptation/test.log", "r").read()
+        context["task_id"] = task_id
+        context["ansi_logging_content"] = ansi_logging_content
+        return context
