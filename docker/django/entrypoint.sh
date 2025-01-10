4926712e68b8b86fab7a5e6c79e103b0d36ea422
Timothe Jost <timothe.jost@wanadoo.fr>
2025-01-10 11:49:52 +0100

    Changelog : - tests with new backup data structure

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -3,14 +3,16 @@ echo -n "Django version:" && echo -n $(pdm run python -m django --version) && ec
 
 pdm install --prod --no-editable --frozen-lockfile
 
-/app/scripts/restore_on_run.sh
-
 echo "Applying database migrations..."
 pdm run ./src/manage.py migrate # --noinput 
 
 # Collect static files
 echo "Collecting static files..."
 pdm run ./src/manage.py collectstatic --noinput --clear --verbosity 0
+
+# Restore
+echo "Restoring postgres table from backup if any"
+pdm run ./src/manage.py dbrestore --noinput
 
 # Start Gunicorn
 echo "Starting Gunicorn to serve django alyx..."
